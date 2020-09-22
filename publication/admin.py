30fa253a03a3b2987905817f5a6d06731a14ed33
nurmanbetov594 <nurmanbetov594@gmail.com>
2020-09-22 18:42:26 +0600

    added models:

---
+++ publication/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display=[
+        "id",
+        "image",
+        "description",
+        "user",
+        "created",
+        "updated",
+        "deleted"
+    ]
+
+    fields = [
+        "image",
+        "description",
+        "user",
+        "deleted"
+    ]
