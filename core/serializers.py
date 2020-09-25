35fa4b913df63bc926e09039ed8ba374f1e9d12b
nurmanbetov594 <nurmanbetov594@gmail.com>
2020-09-25 14:48:06 +0600

    modified profile EP, added subs and dubs list

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers 
 from .models import Profile
 
@@ -23,3 +24,41 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def get_publication_count(self, obj):
         return obj.user.publication.filter(deleted=False).count()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    subscription_count = serializers.SerializerMethodField()
+    subscriber_count = serializers.SerializerMethodField()
+    publication_count = serializers.SerializerMethodField()
+
+
+    class Meta:
+        model = User 
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "subscription_count",
+            "subscriber_count",
+            "publication_count"
+        ]
+
+    def get_subscription_count(self, obj):
+        return obj.profile.subscription.all().count()
+
+    def get_subscriber_count(self, obj):
+        return obj.subscriber.all().count()
+
+    def get_publication_count(self, obj):
+        return obj.publication.filter(deleted=False).count()
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User 
+        fields = [
+            "username",
+            "first_name",
+            "last_name"
+        ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
-from rest_framework.generics import RetrieveAPIView
-from .serializers import ProfileSerializer
+from rest_framework.generics import RetrieveAPIView, \
+    ListAPIView
+from .serializers import *
 from .models import Profile 
 
 
 class ProfileRetrieveView(RetrieveAPIView):
     serializer_class = ProfileSerializer
-    #lookup_field = "user"
+    #lookup_field = "user__name"
     queryset = Profile.objects.all()
+
+
+class UserRetrieveView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    lookup_field = "username"
+    queryset = User.objects.filter(is_active=True)
+
+class SubscribingListView(ListAPIView):
+    serializer_class = UserListSerializer
+
+    def get_queryset(self):
+        username = self.kwargs["username"]
+        user = User.objects.get(username=username)
+        lst = user.profile.subscription.filter(is_active=True)
+        return lst 
+
+class SubscribersListView(ListAPIView):
+    serializer_class = UserListSerializer
+
+    def get_queryset(self):
+        username = self.kwargs.get("username")
+        user = User.objects.get(username=username)
+        lst = User.objects.filter(profile__subscription__in=[user])
+        return lst  
 
 
 
